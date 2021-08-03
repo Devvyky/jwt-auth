cbe5302587d11ced2b1c0679ab425012c1e2beed
devvyky <okanemechibuikem@gmail.com>
2021-08-03 11:17:31 +0100

    fix: deploy fixes

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function dbConfig() {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log("Database connected")
-    } catch (error) {
-        console.log(error)
-    }
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-module.exports = dbConfig
+module.exports = dbConfig;
